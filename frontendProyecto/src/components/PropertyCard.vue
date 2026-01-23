d85f5a65a5fb7dc850f202f53df9cfa731623c7e
frankmg03 <frankMG7u7@hotmail.com>
2026-01-23 17:27:21 -0600

    feat(propiedades): agregar sección de propiedades destacadas

---
+++ frontendProyecto/src/components/PropertyCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <img :src="property.image" />
+    <div class="info">
+      <h3>{{ property.title }}</h3>
+      <p class="price">{{ property.price }}</p>
+      <p>{{ property.location }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  property: Object
+})
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #eee;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+img {
+  width: 100%;
+  height: 180px;
+  object-fit: cover;
+}
+
+.price {
+  color: #c9a23f;
+  font-weight: bold;
+}
+</style>
---
+++ frontendProyecto/src/data/properties.mock.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 1,
+    title: 'Casa moderna',
+    location: 'Ciudad de México',
+    price: 2500000,
+    image: 'https://picsum.photos/400/300'
+  },
+  {
+    id: 2,
+    title: 'Departamento céntrico',
+    location: 'Guadalajara',
+    price: 1800000,
+    image: 'https://picsum.photos/401/300'
+  }
+]
