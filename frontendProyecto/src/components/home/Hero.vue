e41609d3806fb5685c70a6f1cee8d1280fa1882d
frankmg03 <frankMG7u7@hotmail.com>
2026-01-23 17:21:40 -0600

    feat(home): agregar sección hero con imagen y buscador

---
+++ frontendProyecto/src/components/home/Hero.vue
@@ -0,0 +1,35 @@
+<script setup>
+import SearchBar from './SearchBar.vue';
+</script>
+
+<template>
+  <section class="hero">
+    <div class="overlay">
+      <h1>JAKEDA INMOBILIARIA</h1>
+      <p>Asesoría integral con respaldo jurídico para compra, venta y renta</p>
+
+      <!-- BUSCADOR ENCIMADO -->
+      <SearchBar />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.hero {
+  background-image: url('src/assets/images/fondo.png');
+  background-size: cover;
+  background-position: center;
+  height: 35vh;
+  position: relative;
+}
+
+.overlay {
+  background: rgba(0,0,0,.45);
+  height: 100%;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 4rem;
+}
+</style>
---
+++ frontendProyecto/src/components/home/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="search-box">
+    <input placeholder="Ubicación" />
+    <select>
+      <option>Tipo de Propiedad</option>
+    </select>
+    <select>
+      <option>Operación</option>
+    </select>
+    <button>Buscar Propiedades</button>
+  </div>
+</template>
+
+<style scoped>
+.search-box {
+  background: rgba(255, 255, 255, 0);
+  padding: 20px;
+  display: flex;
+  gap: 20px;
+  border-radius: 8px;
+  margin-top: 30px;
+  box-shadow: 0 10px 30px rgba(0,0,0,.2);
+}
+
+input, select {
+  padding: 20px;
+}
+button {
+  background: #f59e0b;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+}
+</style>
