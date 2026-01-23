fec65349258ac65507c03d1fe3db688a07855156
frankmg03 <frankMG7u7@hotmail.com>
2026-01-23 17:31:05 -0600

    feat(layout): agregar navbar y footer globales

---
+++ frontendProyecto/src/components/layout/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <p>© 2024 JAKEDA Inmobiliaria</p>
+  </footer>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.footer {
+  padding: 20px;
+  text-align: center;
+  background: #0f2a44;
+  color: white;
+}
+</style>
---
+++ frontendProyecto/src/components/layout/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="navbar">
+    <div class="logo">JAKEDA</div>
+
+    <nav>
+      <a href="#">Inicio</a>
+      <a href="#">Servicios</a>
+      <a href="#">Propiedades</a>
+      <a href="#">Nosotros</a>
+      <a href="#">Contacto</a>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.navbar {
+  height: 70px;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 40px;
+  box-shadow: 0 2px 10px rgba(0,0,0,.05);
+}
+
+.logo {
+  font-weight: bold;
+  font-size: 50px;
+  color: #f5a623;
+}
+
+nav a {
+  margin-left: 20px;
+  font-size: 30px;
+  text-decoration: none;
+  color: #333;
+  font-weight: 500;
+}
+</style>
